c9612b2675323c691d7735a29c937b82ef402a7b
Amin Boulouma <amin@boulouma.com>
2026-06-18 12:58:36 +0100

    SOLID on Jekyll

--- ai_systems_design/py_jekyll/py_jekyll.py
+++ ai_systems_design/py_jekyll/py_jekyll.py
@@ -1,7 +1,33 @@
-import os
+import os, logging
 
 from ai_systems_design.py_yaml import YAMLParser
 from ai_systems_design.py_markdown_to_html.py_markdown_to_html import MarkdownToHTML
+
+
+class ErrorHandler:
+    @staticmethod
+    def with_error_handling(func):
+        def wrapper(*args, **kwargs):
+            if 'input_dir' in kwargs and not os.path.isdir(kwargs['input_dir']):
+                raise ValueError(f"Input dir {kwargs['input_dir'] } is not a dir.")
+            try: result = func(*args, **kwargs); return result
+            except Exception as e: logging.error(f"Error in {func.__name__}: {e}"); return None
+        return wrapper
+    
+
+class FileOperations:
+    @staticmethod
+    def get_file_paths(md_file_name, input_dir, output_dir):
+        html_file_name = f"{ md_file_name.split('.md')[0] }.html"
+        return tuple(map(lambda x, y: os.path.join(x, y), (input_dir, output_dir), (md_file_name, html_file_name)))
+  
+    @staticmethod
+    @ErrorHandler.with_error_handling
+    def create_output_dir(input_dir):
+        output_dir = os.path.join(os.path.dirname(input_dir), 'output')
+        if not os.path.exists(output_dir): os.makedirs(output_dir, exist_ok=True)
+        return output_dir
+
 
 class LayoutHandler:
     @staticmethod
@@ -18,39 +44,32 @@ class ConfigHandler:
 class HTMLRenderer:
     @staticmethod
     def render_html(layout, config, md_file_path):
-        html, config['content'] = layout, MarkdownToHTML(md_file_path).md_to_html()
+        html = layout.replace('{{ site.content }}', MarkdownToHTML(md_file_path).md_to_html())
         for key in config: html = html.replace('{{ site.' + key + ' }}', config[key])
         return html
     
 
 class HTMLGenerator:
     @staticmethod
+    @ErrorHandler.with_error_handling
     def generate_html_page(layout, config, md_file_path, html_file_path):
-        html = HTMLRenderer.render_html(layout, config, md_file_path)
-        with open(html_file_path, 'wb') as html: html.write(layout.encode('utf-8'))
+        with open(html_file_path, 'wb') as html: html.write(HTMLRenderer.render_html(layout, config, md_file_path).encode('utf-8'))
 
     @staticmethod
-    def generate_html_pages(input_folder, output_folder, layout, config):
-        for md_file_name in os.listdir(input_folder):
-            md_file_path, html_file_path = DependenciesManager.get_files_paths(md_file_name, input_folder, output_folder)
+    @ErrorHandler.with_error_handling
+    def generate_html_pages(input_dir, output_dir, layout, config):
+        for md_file_name in os.listdir(input_dir):
+            md_file_path, html_file_path = FileOperations.get_file_paths(md_file_name, input_dir, output_dir)
             if os.path.isfile(md_file_path): HTMLGenerator.generate_html_page(layout, config, md_file_path, html_file_path)
 
 
 class DependenciesManager:
     @staticmethod
-    def get_dependencies(layout_path, config_file_path):
-        return LayoutHandler.get_layout(layout_path), ConfigHandler.get_config_mapping(config_file_path)
+    def get_dependencies(input_dir, layout_path, config_file_path):
+        return input_dir, FileOperations.create_output_dir(input_dir), LayoutHandler.get_layout(layout_path), ConfigHandler.get_config_mapping(config_file_path)
 
-    @staticmethod
-    def get_files_paths(md_file_name, input_folder, output_folder):
-        html_file_name = f"{ md_file_name.split('.md')[0] }.html"
-        return tuple(map(lambda x, y: os.path.join(x, y), (input_folder, output_folder), (md_file_name, html_file_name)))
-    
 
 class Jekyll:
-    def __init__(self, input_folder, output_folder, layout_path, config_file_path):
-        self.input_folder, self.output_folder = input_folder, output_folder 
-        self.layout, self.config = DependenciesManager.get_dependencies(layout_path, config_file_path)
-
-    def generate_site(self):
-        HTMLGenerator.generate_html_pages(self.input_folder, self.output_folder, self.layout, self.config)
+    @staticmethod
+    def generate_site(input_dir, layout_path, config_file_path):
+        HTMLGenerator.generate_html_pages(*DependenciesManager.get_dependencies(input_dir, layout_path, config_file_path))
--- main.py
+++ main.py
@@ -2,11 +2,8 @@ from ai_systems_design.py_jekyll.py_jekyll import Jekyll
 from ai_systems_design.py_slug_generator import SlugGenerator, TerminalInterface
 
 def generate_jekyll_site():
-    jekyll = Jekyll('ai_systems_design/py_jekyll/input', 
-                    'ai_systems_design/py_jekyll/output', 
-                    'ai_systems_design/py_jekyll/layout.html', 
-                    'ai_systems_design/py_jekyll/config.yaml')
-    jekyll.generate_site()
+    base_path = 'ai_systems_design/py_jekyll/'
+    Jekyll.generate_site(f'{base_path}input', f'{base_path}layout.html', f'{base_path}config.yaml')
 
 def generate_slugs():
     slug_engine = SlugGenerator()
